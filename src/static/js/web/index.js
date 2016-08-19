b91bf61e68abe0b17283643acba7b38715e52371
wangweixi <wangweixi@uzhu.com.cn>
2016-08-19 18:54:16 +0800

    fix background on mobile

--- src/static/js/web/index.js
+++ src/static/js/web/index.js
@@ -1,23 +1,25 @@
 $(function() {
-    $("body").removeClass("ms-controller").vegas({
-        slides: [
-            {src: "/img/web/bg.jpg"},
-        ]
-    });
 
-    if(Env.isIE()) {
-        $("#loading").hide();
-        return;
+    if(!Env.isMobile()) {
+        $("body").removeClass("ms-controller").vegas({
+            slides: [
+                {src: "/img/web/bg.jpg"},
+            ]
+        });
     }
 
-    if(Env.isMobile()) {
-        $("#loading").hide();
-        Pano.initPic("http://obfgoys2n.bkt.clouddn.com/environment.jpg");
-    } else {
-        var video_src = "http://obfgoys2n.bkt.clouddn.com/xiaomi_VR_preview_727_2.mp4";
-        Preload.add(video_src, function() {
+    if(!Env.isIE()) {
+        if(Env.isMobile()) {
             $("#loading").hide();
-            Pano.initVideo(video_src);
-        });
+            Pano.initPic("http://obfgoys2n.bkt.clouddn.com/environment.jpg");
+        } else {
+            var video_src = "http://obfgoys2n.bkt.clouddn.com/xiaomi_VR_preview_727_2.mp4";
+            Preload.add(video_src, function() {
+                $("#loading").hide();
+                Pano.initVideo(video_src);
+            });
+        }
+    } else {
+        $("#loading").hide();
     }
 });
