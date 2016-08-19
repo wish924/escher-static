642aa3c3693df4967794c6fa77936e2d0b2e178b
wangweixi <wangweixi@uzhu.com.cn>
2016-08-19 18:03:31 +0800

    release build

---
+++ release/static/js/web/index-92809a349e.js
@@ -0,0 +1,1 @@
+$(function(){if((Env.isAndroid||Env.isiOS)&&(window.location.href="http://m.es.wangweixi.greensfordev.com"),$("body").removeClass("ms-controller").vegas({slides:[{src:"/img/web/bg.jpg"}]}),Env.isIE(),!0)return void $("#loading").hide();var i="http://obfgoys2n.bkt.clouddn.com/xiaomi_VR_preview_727_2.mp4";Preload.add(i,function(){$("#loading").hide(),Pano.initVideo(i)})});
